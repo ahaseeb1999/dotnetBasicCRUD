ef61c0ec41b7004a297f0c121441163daa2b322e
3TEE <3TEE@DESKTOP-IABU8I9>
2024-03-06 11:10:16 +0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDFormsApp01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-IABU8I9\\SQLEXPRESS;Initial Catalog=CRUDFormApp01;Integrated Security=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("Insert into Table_1 values(@id, @Name, @Age)", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@Name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@Age", double.Parse(textBox3.Text));
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            MessageBox.Show("Successfully Submitted");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-IABU8I9\\SQLEXPRESS;Initial Catalog=CRUDFormApp01;Integrated Security=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("Update Table_1 set Name=@Name, Age=@Age where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@Name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@Age", double.Parse(textBox3.Text));
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully Updated");
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-IABU8I9\\SQLEXPRESS;Initial Catalog=CRUDFormApp01;Integrated Security=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("Delete Table_1 where id='"+int.Parse(textBox1.Text)+"' ", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully Deleted");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-IABU8I9\\SQLEXPRESS;Initial Catalog=CRUDFormApp01;Integrated Security=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("Select * from Table_1 where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
